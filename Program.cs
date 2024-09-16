6aa49f5fd2f8c2cfdf4ee7887864f670422182e5
khosh <khosh@SOFIA>
2024-09-16 23:17:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿string slumpTal = "81575187k62387623593465387469";
+long totalSumma = 0;
+
+
+for (int i = 0; i < slumpTal.Length; i++)
+{
+
+    if (char.IsDigit(slumpTal[i]))
+    {
+        char startSiffra = slumpTal[i];
+
+        for (int j = i + 1; j < slumpTal.Length; j++)
+        {
+            if (slumpTal[j] == startSiffra)
+            {
+                bool allaSiffror = true;
+                bool ingaAndraStartSlutSiffror = true;
+
+                for (int k = i + 1; k < j; k++)
+                {
+                    if (!char.IsDigit(slumpTal[k]))
+                    {
+                        allaSiffror = false;
+                        break;
+                    }
+                    if (slumpTal[k] == startSiffra)
+                    {
+                        ingaAndraStartSlutSiffror = false;
+                        break;
+                    }
+                }
+
+                if (allaSiffror && ingaAndraStartSlutSiffror)
+                {
+                    string matchandeDel = slumpTal.Substring(i, (j - i) + 1);
+                    totalSumma += long.Parse(matchandeDel);
+
+                    Console.WriteLine(slumpTal.Substring(0, i));
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(matchandeDel);
+                    Console.ResetColor();
+                    Console.WriteLine(slumpTal.Substring(j + 1));
+                }
+            }
+        }
+
+    }
+}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Totala summan adderad blir: {totalSumma}");
+Console.ResetColor();
+
+Console.WriteLine("Tryck på valfri tnagent för att avsluta");
+Console.ReadKey();
